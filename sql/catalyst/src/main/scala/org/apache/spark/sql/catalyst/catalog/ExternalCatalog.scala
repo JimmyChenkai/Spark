519a9c63c9b9e0be8e89a5514a7a39857ad25afd
JimmyChenkai <chenkaiscu@sina.com>
2019-06-29 21:22:01 +0800

    Update ExternalCatalog.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalog.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalog.scala
@@ -22,39 +22,40 @@ import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.types.StructType
 
 /**
- * Interface for the system catalog (of functions, partitions, tables, and databases).
+ * system catalog（方法，分区，表，数据库）的接口
+ * 
+ * 这个接口是为非临时的items提供，同时接口的继承类们一定是线程安全的，
+ * 由于接口是需要用到多线程。
+ * 这是一个外部的catalog因为是需要和外部的系统进行交互
  *
- * This is only used for non-temporary items, and implementations must be thread-safe as they
- * can be accessed in multiple threads. This is an external catalog because it is expected to
- * interact with external systems.
- *
- * Implementations should throw [[NoSuchDatabaseException]] when databases don't exist.
+ * 如果数据库不存在的时候，实现接口的类或者方法一定要抛出[[NoSuchDatabaseException]]异常
  */
 trait ExternalCatalog {
   import CatalogTypes.TablePartitionSpec
 
   // --------------------------------------------------------------------------
-  // Utils
+  // 通用 层面
   // --------------------------------------------------------------------------
 
+  //判断数据库是否存在
   protected def requireDbExists(db: String): Unit = {
     if (!databaseExists(db)) {
       throw new NoSuchDatabaseException(db)
     }
   }
-
+  //判断数据表是否存在
   protected def requireTableExists(db: String, table: String): Unit = {
     if (!tableExists(db, table)) {
       throw new NoSuchTableException(db = db, table = table)
     }
   }
-
+  //判断方法是存在
   protected def requireFunctionExists(db: String, funcName: String): Unit = {
     if (!functionExists(db, funcName)) {
       throw new NoSuchFunctionException(db = db, func = funcName)
     }
   }
-
+  //判断方法不存在
   protected def requireFunctionNotExists(db: String, funcName: String): Unit = {
     if (functionExists(db, funcName)) {
       throw new FunctionAlreadyExistsException(db = db, func = funcName)
@@ -62,44 +63,44 @@ trait ExternalCatalog {
   }
 
   // --------------------------------------------------------------------------
-  // Databases
+  // 数据库 层面
   // --------------------------------------------------------------------------
-
+  
+  //创建数据库，成功就放回true，失败则返回false
   def createDatabase(dbDefinition: CatalogDatabase, ignoreIfExists: Boolean): Unit
-
+  //删除数据库，成功就返回tru，失败就返回false
   def dropDatabase(db: String, ignoreIfNotExists: Boolean, cascade: Boolean): Unit
 
   /**
-   * Alter a database whose name matches the one specified in `dbDefinition`,
-   * assuming the database exists.
+   * 如果数据存在的话，可以修改哪些已经在`dbDefinition`明确定义过的数据库
    *
-   * Note: If the underlying implementation does not support altering a certain field,
-   * this becomes a no-op.
+   * 注意: 如果实现类中在底层不支持修改某个字段，那么就是禁止的
    */
   def alterDatabase(dbDefinition: CatalogDatabase): Unit
-
+  //获取数据名字
   def getDatabase(db: String): CatalogDatabase
-
+  //数据是否存在
   def databaseExists(db: String): Boolean
-
+  //列出所有数据库
   def listDatabases(): Seq[String]
-
+  //根据指定规则pattern列出所有数据库
   def listDatabases(pattern: String): Seq[String]
-
+  //设置当前数据库
   def setCurrentDatabase(db: String): Unit
 
   // --------------------------------------------------------------------------
-  // Tables
+  // 数据表 层面
   // --------------------------------------------------------------------------
 
+  //创建一张表，如果创建成功就返回tru，否则返回false
   def createTable(tableDefinition: CatalogTable, ignoreIfExists: Boolean): Unit
-
+  //删除数据表，入参是db[数据库],table[表],ignorenIfNostExists[是否有if not exists判断],pure[没咋看懂~~]
   def dropTable(
       db: String,
       table: String,
       ignoreIfNotExists: Boolean,
       purge: Boolean): Unit
-
+  //重命名数据表，入参是db[数据库],oldName[之前表名称],newName[新表名称]
   def renameTable(db: String, oldName: String, newName: String): Unit
 
   /**
@@ -113,31 +114,31 @@ trait ExternalCatalog {
   def alterTable(tableDefinition: CatalogTable): Unit
 
   /**
-   * Alter the data schema of a table identified by the provided database and table name. The new
-   * data schema should not have conflict column names with the existing partition columns, and
-   * should still contain all the existing data columns.
+   * 更改由提供的数据库和表名标识的表的数据结构。
+   * 新的数据结构不应具有与现有分区列冲突的列名, and
+   * 并且仍应包含所有现有数据列。
    *
-   * @param db Database that table to alter schema for exists in
-   * @param table Name of table to alter schema for
-   * @param newDataSchema Updated data schema to be used for the table.
+   * @param db 数据库 
+   * @param table 表
+   * @param newDataSchema 新的表结构schema
    */
   def alterTableDataSchema(db: String, table: String, newDataSchema: StructType): Unit
 
-  /** Alter the statistics of a table. If `stats` is None, then remove all existing statistics. */
+  //更改表的统计信息。如果“stats”为“none”，则删除所有现有统计。
   def alterTableStats(db: String, table: String, stats: Option[CatalogStatistics]): Unit
-
+  //获取单个表，入参db[数据库],table[表]
   def getTable(db: String, table: String): CatalogTable
-
+  //获取一个以上表，入参db[数据库],tables[数据表集合]
   def getTablesByName(db: String, tables: Seq[String]): Seq[CatalogTable]
-
+  //判断表是否存在
   def tableExists(db: String, table: String): Boolean
-
+  //列出指定数据库下所有表
   def listTables(db: String): Seq[String]
-
+  //根据指定规则pattern列出指定数据库下的所有表
   def listTables(db: String, pattern: String): Seq[String]
 
   /**
-   * Loads data into a table.
+   * 数据落入表中
    *
    * @param isSrcLocal Whether the source data is local, as defined by the "LOAD DATA LOCAL"
    *                   HiveQL command.
@@ -150,7 +151,7 @@ trait ExternalCatalog {
       isSrcLocal: Boolean): Unit
 
   /**
-   * Loads data into a partition.
+   * 数据落入分区
    *
    * @param isSrcLocal Whether the source data is local, as defined by the "LOAD DATA LOCAL"
    *                   HiveQL command.
@@ -173,15 +174,16 @@ trait ExternalCatalog {
       numDP: Int): Unit
 
   // --------------------------------------------------------------------------
-  // Partitions
+  // 分区 层面
   // --------------------------------------------------------------------------
 
+  //创建一个分区
   def createPartitions(
       db: String,
       table: String,
       parts: Seq[CatalogTablePartition],
       ignoreIfExists: Boolean): Unit
-
+  //删除一个分区
   def dropPartitions(
       db: String,
       table: String,
@@ -191,8 +193,8 @@ trait ExternalCatalog {
       retainData: Boolean): Unit
 
   /**
-   * Override the specs of one or many existing table partitions, assuming they exist.
-   * This assumes index i of `specs` corresponds to index i of `newSpecs`.
+   * 覆盖一个或多个现有表分区的规范（假设它们存在）。
+   * 这假定“specs”的索引i对应于“newspecs”的索引i
    */
   def renamePartitions(
       db: String,
@@ -201,21 +203,20 @@ trait ExternalCatalog {
       newSpecs: Seq[TablePartitionSpec]): Unit
 
   /**
-   * Alter one or many table partitions whose specs that match those specified in `parts`,
-   * assuming the partitions exist.
+   * 更改其规范与“parts”中指定的规范匹配的一个或多个表分区，
+   * 假设存在的分区。
    *
-   * Note: If the underlying implementation does not support altering a certain field,
-   * this becomes a no-op.
+   * 注意: 如果底层实现类不支持分，则为禁止状态
    */
   def alterPartitions(
       db: String,
       table: String,
       parts: Seq[CatalogTablePartition]): Unit
-
+  //获取分区
   def getPartition(db: String, table: String, spec: TablePartitionSpec): CatalogTablePartition
 
   /**
-   * Returns the specified partition or None if it does not exist.
+   * 返回指定的分区，如果不存在，则返回“无”。
    */
   def getPartitionOption(
       db: String,
@@ -223,20 +224,20 @@ trait ExternalCatalog {
       spec: TablePartitionSpec): Option[CatalogTablePartition]
 
   /**
-   * List the names of all partitions that belong to the specified table, assuming it exists.
+   * 列出所有分区
    *
-   * For a table with partition columns p1, p2, p3, each partition name is formatted as
-   * `p1=v1/p2=v2/p3=v3`. Each partition column name and value is an escaped path name, and can be
-   * decoded with the `ExternalCatalogUtils.unescapePathName` method.
+   * 对于具有分区列p1、p2、p3的表，每个分区名的格式为
+   * ` p1=v1/p2=v2/p3=v3`。每个分区列名和值都是转义的路径名，可以
+   * 用“externalCatalogUtils.unescapathName”方法解码。
    *
-   * The returned sequence is sorted as strings.
+   * 返回是一个string集合
    *
-   * A partial partition spec may optionally be provided to filter the partitions returned, as
-   * described in the `listPartitions` method.
+   * 可以选择提供部分分区规范来过滤返回的分区, 
+   * listPartitions` 方法中明细一样
    *
-   * @param db database name
-   * @param table table name
-   * @param partialSpec partition spec
+   * @param db 数据库
+   * @param table 表
+   * @param partialSpec 分区
    */
   def listPartitionNames(
       db: String,
@@ -244,15 +245,15 @@ trait ExternalCatalog {
       partialSpec: Option[TablePartitionSpec] = None): Seq[String]
 
   /**
-   * List the metadata of all partitions that belong to the specified table, assuming it exists.
+   * 列出属于指定表的所有分区的元数据（假定该表存在）。
    *
-   * A partial partition spec may optionally be provided to filter the partitions returned.
-   * For instance, if there exist partitions (a='1', b='2'), (a='1', b='3') and (a='2', b='4'),
-   * then a partial spec of (a='1') will return the first two only.
+   * 可以选择提供部分分区规范来过滤返回的分区。
+   * 如果存在分区（A=1'，B=2'，（A=1'，B=3'）和（A=2'，B=4'），
+   * 然后（a='1'）的部分规范将只返回前两个。
    *
-   * @param db database name
-   * @param table table name
-   * @param partialSpec partition spec
+   * @param db 数据库
+   * @param table 表名称
+   * @param partialSpec 分区
    */
   def listPartitions(
       db: String,
@@ -260,12 +261,12 @@ trait ExternalCatalog {
       partialSpec: Option[TablePartitionSpec] = None): Seq[CatalogTablePartition]
 
   /**
-   * List the metadata of partitions that belong to the specified table, assuming it exists, that
-   * satisfy the given partition-pruning predicate expressions.
+   * 列出属于指定表的分区的元数据（假定该表存在）
+   * 满足给定的分区修剪谓词表达式。【什么是谓词表达式？】
    *
-   * @param db database name
-   * @param table table name
-   * @param predicates partition-pruning predicates
+   * @param db 数据库
+   * @param table 表名称
+   * @param predicates 分区修剪谓词
    * @param defaultTimeZoneId default timezone id to parse partition values of TimestampType
    */
   def listPartitionsByFilter(
@@ -275,20 +276,21 @@ trait ExternalCatalog {
       defaultTimeZoneId: String): Seq[CatalogTablePartition]
 
   // --------------------------------------------------------------------------
-  // Functions
+  // 方法 层面
   // --------------------------------------------------------------------------
 
+  //创建方法，入参db[数据库],funcDefinition[方法]
   def createFunction(db: String, funcDefinition: CatalogFunction): Unit
-
+  //删除方法,入参db[数据库],funcName[方法名称]
   def dropFunction(db: String, funcName: String): Unit
-
+  //修改一个方法,入参db[数据库],fuincDefinition[方法体]
   def alterFunction(db: String, funcDefinition: CatalogFunction): Unit
-
+  //重命名方法,入参db[数据库],oldName[旧名称],newName[新名称]
   def renameFunction(db: String, oldName: String, newName: String): Unit
-
+  //获取一个方法体，入参db[数据库],funcNamep[方法名称]
   def getFunction(db: String, funcName: String): CatalogFunction
-
+  //判断方法是否存在，入参db[数据库],funcName[方法名称]
   def functionExists(db: String, funcName: String): Boolean
-
+  //根据指定规则列出所有方法，入参db[数据库]，parttern[指定规则]
   def listFunctions(db: String, pattern: String): Seq[String]
 }
