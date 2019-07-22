8689d82db7f640b50275dffd5a697a0480e1d00c
JimmyChenkai <chenkaiscu@sina.com>
2019-07-22 22:14:32 +0800

    Update ComplexTypes.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/ComplexTypes.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/ComplexTypes.scala
@@ -22,40 +22,39 @@ import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, LogicalPlan}
 import org.apache.spark.sql.catalyst.rules.Rule
 
 /**
- * Simplify redundant [[CreateNamedStructLike]], [[CreateArray]] and [[CreateMap]] expressions.
+ * 简化冗余的[[createnamedstructlike]]、[[createArray]]和[[createMap]]表达式。
  */
 object SimplifyExtractValueOps extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan transform {
-    // One place where this optimization is invalid is an aggregation where the select
-    // list expression is a function of a grouping expression:
+    // 
+    // 此优化无效的一个地方是聚合，其中选择列表表达式是分组表达式的函数：
     //
     // SELECT struct(a,b).a FROM tbl GROUP BY struct(a,b)
     //
-    // cannot be simplified to SELECT a FROM tbl GROUP BY struct(a,b). So just skip this
-    // optimization for Aggregates (although this misses some cases where the optimization
-    // can be made).
+    // 不可简化成 SELECT a FROM tbl GROUP BY struct(a,b).
+    // 因此，只需跳过对聚合的优化（尽管这遗漏了一些可以进行优化的情况）。
     case a: Aggregate => a
     case p => p.transformExpressionsUp {
-      // Remove redundant field extraction.
+      // 删除冗余字段提取。
       case GetStructField(createNamedStructLike: CreateNamedStructLike, ordinal, _) =>
         createNamedStructLike.valExprs(ordinal)
 
-      // Remove redundant array indexing.
+      // 删除冗余阵列索引。
       case GetArrayStructFields(CreateArray(elems), field, ordinal, _, _) =>
-        // Instead of selecting the field on the entire array, select it from each member
-        // of the array. Pushing down the operation this way may open other optimizations
-        // opportunities (i.e. struct(...,x,...).x)
+        // 不要在整个数组中选择字段，而是从数组的每个成员中选择它。
+        // 
+        // 以这种方式下推操作可能会打开其他优化机会（即struct（…，x，….x）
         CreateArray(elems.map(GetStructField(_, ordinal, Some(field.name))))
 
-      // Remove redundant map lookup.
+      // 删除冗余映射查找。
       case ga @ GetArrayItem(CreateArray(elems), IntegerLiteral(idx)) =>
-        // Instead of creating the array and then selecting one row, remove array creation
-        // altogether.
+       
+        // 不创建数组，然后选择一行，而是完全删除数组创建。
         if (idx >= 0 && idx < elems.size) {
-          // valid index
+          // 有效索引
           elems(idx)
         } else {
-          // out of bounds, mimic the runtime behavior and return null
+          // 越界，模拟运行时行为并返回空值
           Literal(null, ga.dataType)
         }
       case GetMapValue(CreateMap(elems), key) => CaseKeyWhen(key, elems)
