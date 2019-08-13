ec098f9175da0094fd8dd5ed0d8ab7b1782e9a6d
JimmyChenkai <chenkaiscu@sina.com>
2019-08-13 23:22:07 +0800

    Update LocalTableScanExec.scala

--- sql/core/src/main/scala/org/apache/spark/sql/execution/LocalTableScanExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/LocalTableScanExec.scala
@@ -24,32 +24,31 @@ import org.apache.spark.sql.execution.metric.SQLMetrics
 
 
 /**
- * Physical plan node for scanning data from a local collection.
- *
- * `Seq` may not be serializable and ideally we should not send `rows` and `unsafeRows`
- * to the executors. Thus marking them as transient.
+ *`seq`可能不是可序列化的，理想情况下我们不应该发送`rows`和`unsafeRows`执行人。因此将它们标记为瞬态。
  */
 case class LocalTableScanExec(
     output: Seq[Attribute],
     @transient rows: Seq[InternalRow]) extends LeafExecNode with InputRDDCodegen {
-
+  //重写了metircs    
   override lazy val metrics = Map(
     "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"))
-
+  //懒加载机制
   @transient private lazy val unsafeRows: Array[InternalRow] = {
+    //如果rows是空值，返回空Array  
     if (rows.isEmpty) {
       Array.empty
+    //否则UnsageProjection创建    
     } else {
       val proj = UnsafeProjection.create(output, output)
       rows.map(r => proj(r).copy()).toArray
     }
   }
-
+  //懒加载机制 获取分区树
   private lazy val numParallelism: Int = math.min(math.max(unsafeRows.length, 1),
     sqlContext.sparkContext.defaultParallelism)
-
+  //RDD变量载体
   private lazy val rdd = sqlContext.sparkContext.parallelize(unsafeRows, numParallelism)
-
+  //重写doExecute方法，返回一个RDD[InternalRow]
   protected override def doExecute(): RDD[InternalRow] = {
     val numOutputRows = longMetric("numOutputRows")
     rdd.map { r =>
@@ -57,7 +56,7 @@ case class LocalTableScanExec(
       r
     }
   }
-
+  //覆盖重写StringArgs
   override protected def stringArgs: Iterator[Any] = {
     if (rows.isEmpty) {
       Iterator("<empty>", output)
@@ -65,22 +64,22 @@ case class LocalTableScanExec(
       Iterator(output)
     }
   }
-
+  //重写了executeCollect
   override def executeCollect(): Array[InternalRow] = {
     longMetric("numOutputRows").add(unsafeRows.size)
     unsafeRows
   }
-
+  //覆盖重写了executeTake
   override def executeTake(limit: Int): Array[InternalRow] = {
     val taken = unsafeRows.take(limit)
     longMetric("numOutputRows").add(taken.size)
     taken
   }
 
-  // Input is already UnsafeRows.
+  // 输入已经是UnsafeRows。
   override protected val createUnsafeProjection: Boolean = false
 
-  // Do not codegen when there is no parent - to support the fast driver-local collect/take paths.
+  // 当没有父代时，不要编码 - 支持快速驱动程序本地收集/获取路径。
   override def supportCodegen: Boolean = (parent != null)
 
   override def inputRDD: RDD[InternalRow] = rdd
